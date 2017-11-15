ce2c6e885ce28a0297f154d40b7a99c9cfa44a14
helthbrazil <hebertalvesferreira@gmail.com>
2017-11-15 00:13:23 -0200

    Reiniciar

--- src/ConversorRomano.java
+++ src/ConversorRomano.java
@@ -4,52 +4,6 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 public class ConversorRomano {
-	private Map<Integer, String> algarismos;
-
-	public ConversorRomano() {
-		inicializarAlgarismos();
-	}
-
-	public void inicializarAlgarismos() {
-		this.algarismos = new HashMap<>();
-		this.algarismos.put(1, "I");
-		this.algarismos.put(5, "V");
-		this.algarismos.put(10, "X");
-		this.algarismos.put(50, "L");
-		this.algarismos.put(100, "C");
-		this.algarismos.put(500, "D");
-		this.algarismos.put(100, "M");
-	}
-
-	public String converterNumero(Integer numero) {
-
-		int check = numero;
-
-		while (check > 0) {
-
-		}
-
-		return this.algarismos.get(numero);
-	}
-
-	public void buscarMaiorAlgarismoQueAtenda(int numero) {
-		Iterator it = this.algarismos.entrySet().iterator();
-		while (it.hasNext()) {
-			Map.Entry pair = (Map.Entry) it.next();
-			
-			System.out.println(pair.getKey() + " = " + pair.getValue());
-		}
-	}
-
-	public Integer verificarAlgarismo(String algarismo) {
-		String algarismoFormatado = algarismo.toUpperCase();
-
-		for (Entry<Integer, String> entry : this.algarismos.entrySet()) {
-			if (entry.getValue().equals(algarismoFormatado)) {
-				return entry.getKey();
-			}
-		}
-		return null;
-	}
+	
 
 }
--- src/TesteRomano.java
+++ src/TesteRomano.java
@@ -8,30 +8,9 @@ public class TesteRomano {
 
 	@Test
 	public void testeConversao() {
-		ConversorRomano conversor = new ConversorRomano();
-		String numeroRomano = conversor.converterNumero(1);
-		Assert.assertEquals("I", numeroRomano);
+
 	}
 
-	@Test
-	public void verificarAlgarismoI() {
-		ConversorRomano conversor = new ConversorRomano();
-		int numero = conversor.verificarAlgarismo("i");
-		Assert.assertEquals(1, numero);
-	}
 	
-	@Test
-	public void verificarAlgarismoV() {
-		ConversorRomano conversor = new ConversorRomano();
-		int numero = conversor.verificarAlgarismo("v");
-		Assert.assertEquals(5, numero);
-	}
-	
-	@Test
-	public void verificarAlgarismoX() {
-		ConversorRomano conversor = new ConversorRomano();
-		int numero = conversor.verificarAlgarismo("X");
-		Assert.assertEquals(10, numero);
-	}
 	
 }
